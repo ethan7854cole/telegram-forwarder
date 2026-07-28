64ba5d17abec69a5de38af639109a12701741e39
Lomus Luitel <lomus@Lomuss-MacBook-Pro.local>
2026-07-28 17:56:04 +0545

    Added keyword filter: Total in

--- forwarder.py
+++ forwarder.py
@@ -4,40 +4,22 @@ BOT_TOKEN = "8614082158:AAHIPZpqVvy4EzSKBJmFYBaVst-xtj4m-l0"
 SOURCE_GROUP_ID = -1004330276394
 TARGET_GROUP_ID = -5580596463
 
+KEYWORDS = ["Total in"]
+
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def forward_text(message):
     if message.chat.id == SOURCE_GROUP_ID:
-        try:
-            bot.send_message(TARGET_GROUP_ID, message.text)
-            print('Message sent!')
-        except Exception as e:
-            print('Error: ' + str(e))
+        text = message.text.lower()
+        if any(kw.lower() in text for kw in KEYWORDS):
+            try:
+                bot.send_message(TARGET_GROUP_ID, message.text)
+                print('Message forwarded!')
+            except Exception as e:
+                print('Error: ' + str(e))
+        else:
+            print('Skipped - no keyword match')
 
-@bot.message_handler(content_types=['photo'])
-def forward_photo(message):
-    if message.chat.id == SOURCE_GROUP_ID:
-        try:
-            bot.send_photo(TARGET_GROUP_ID, message.photo[-1].file_id, caption=message.caption)
-        except Exception as e:
-            print('Error: ' + str(e))
-
-@bot.message_handler(content_types=['video'])
-def forward_video(message):
-    if message.chat.id == SOURCE_GROUP_ID:
-        try:
-            bot.send_video(TARGET_GROUP_ID, message.video.file_id, caption=message.caption)
-        except Exception as e:
-            print('Error: ' + str(e))
-
-@bot.message_handler(content_types=['document'])
-def forward_document(message):
-    if message.chat.id == SOURCE_GROUP_ID:
-        try:
-            bot.send_document(TARGET_GROUP_ID, message.document.file_id, caption=message.caption)
-        except Exception as e:
-            print('Error: ' + str(e))
-
-print('Bot is running...')
+print('Bot is running with keyword filter...')
 bot.polling(none_stop=True, allowed_updates=['message'])
