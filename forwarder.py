cfa7264cf55e65e12d863342f30520066843480f
Lomus Luitel <lomus@Lomuss-MacBook-Pro.local>
2026-07-29 11:23:56 +0545

    Updated keyword to You received

--- forwarder.py
+++ forwarder.py
@@ -1,25 +1,28 @@
 import telebot
 
 BOT_TOKEN = "8614082158:AAHIPZpqVvy4EzSKBJmFYBaVst-xtj4m-l0"
-SOURCE_GROUP_ID = -1004330276394
-TARGET_GROUP_ID = -5580596463
 
-KEYWORDS = ["Total in"]
+KEYWORDS = ["You received"]
+
+FORWARD_RULES = {
+    -1004330276394: [-5580596463],
+}
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def forward_text(message):
-    if message.chat.id == SOURCE_GROUP_ID:
+    if message.chat.id in FORWARD_RULES:
         text = message.text.lower()
         if any(kw.lower() in text for kw in KEYWORDS):
-            try:
-                bot.send_message(TARGET_GROUP_ID, message.text)
-                print('Message forwarded!')
-            except Exception as e:
-                print('Error: ' + str(e))
+            for target in FORWARD_RULES[message.chat.id]:
+                try:
+                    bot.send_message(target, message.text)
+                    print('Message forwarded!')
+                except Exception as e:
+                    print('Error: ' + str(e))
         else:
             print('Skipped - no keyword match')
 
-print('Bot is running with keyword filter...')
+print('Bot is running...')
 bot.polling(none_stop=True, allowed_updates=['message'])
